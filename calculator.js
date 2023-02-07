44833f23a75354ec78d32629f2723b38a1e312ae
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-02-07 10:40:59 -0800

    CalCULATOR

--- calculator.js
+++ calculator.js
@@ -1,5 +1,15 @@
-function addInputs() {
-    var inputTotal = +document.getElementById('input1').value + +document.getElementById('input2').value + +document.getElementById('input3').value + +document.getElementById('input4').value;
-    var grossIncome = document.getElementById("totalInput");
+setInterval(function addInputs() {
+    const inputTotal = +document.getElementById('input1').value + +document.getElementById('input2').value + +document.getElementById('input3').value + +document.getElementById('input4').value;
+    const grossIncome = document.getElementById('totalInput');
     grossIncome.textContent = inputTotal;
-}   
+}  , 100)
+
+setInterval(function monthlyIncome (){
+    const monthlyIncome = document.getElementById('annualInput').value / 12;
+    const monthlyOutput = document.getElementById('monthlyOutput');
+    monthlyOutput.textContent = monthlyIncome;
+}, 100)
+
+function calculateReductions (){
+    // const totalReductions = 
+}
