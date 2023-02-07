843c9bf97003a154d481f91708477aaa578cc0b6
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-02-07 09:08:36 -0800

    JAVASCRIPT IS HARD pt. 2

--- calculator.js
+++ calculator.js
@@ -1,7 +1,5 @@
-
 function addInputs() {
-    var inputOne = document.getElementById('input1');
-    alert(inputOne.value);  
-}
-
-addInputs ();
+    var inputTotal = +document.getElementById('input1').value + +document.getElementById('input2').value + +document.getElementById('input3').value + +document.getElementById('input4').value;
+    var grossIncome = document.getElementById("totalInput");
+    grossIncome.textContent = inputTotal;
+}   
