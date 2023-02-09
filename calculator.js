0547356b70e6ff466734e0e199595e4aa8e3923a
AydenCortez <116187730+AydenCortez@users.noreply.github.com>
2023-02-09 09:27:51 -0800

    DECORATION AND OTHER THINGS

--- calculator.js
+++ calculator.js
@@ -6,26 +6,30 @@ setInterval(function addInputs() {
 
 setInterval(function monthlyIncome (){
     // value variable 
-    const monthlyIncome = (document.getElementById('annualInput').value / 12).toLocaleString();
+    const monthlyIncome = ((document.getElementById('annualInput').value / 12).toFixed(2)).toLocaleString();
     const monthlyOutput = document.getElementById('monthlyOutput');
     // text output
     monthlyOutput.textContent = "$" + monthlyIncome;
 }, 100)
 
 function calculateReductions (){
-    const federalTax = document.getElementById('federalTax'); federalTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.12)).toLocaleString();
-    const stateTax = document.getElementById('stateTax'); stateTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.07)).toLocaleString();
-    const securityTax = document.getElementById('securityTax'); securityTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.062)).toLocaleString();
-    const medicareTax = document.getElementById('medicareTax'); medicareTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.0145)).toLocaleString();
-    const disabilityTax = document.getElementById('disabilityTax'); disabilityTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.01)).toLocaleString();
-    const retirementTax = document.getElementById('retirementTax'); retirementTax.textContent = "$" + (Math.round((document.getElementById('annualInput').value / 12) * 0.05)).toLocaleString();
+    // reduction values and text content
+    const federalTax = document.getElementById('federalTax'); federalTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.12).toFixed(2)).toLocaleString();
+    const stateTax = document.getElementById('stateTax'); stateTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.07).toFixed(2)).toLocaleString();
+    const securityTax = document.getElementById('securityTax'); securityTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.062).toFixed(2)).toLocaleString();
+    const medicareTax = document.getElementById('medicareTax'); medicareTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.0145).toFixed(2)).toLocaleString();
+    const disabilityTax = document.getElementById('disabilityTax'); disabilityTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.01).toFixed(2)).toLocaleString();
+    const retirementTax = document.getElementById('retirementTax'); retirementTax.textContent = "$" + (((document.getElementById('annualInput').value / 12) * 0.05).toFixed(2)).toLocaleString();
     const medicalTax = document.getElementById('medicalTax'); medicalTax.textContent = "$" + 180;
-    const totalReductions = (Math.round((document.getElementById('annualInput').value / 12) * 0.12)) + (Math.round((document.getElementById('annualInput').value / 12) * 0.07)) + (Math.round((document.getElementById('annualInput').value / 12) * 0.062)) + (Math.round((document.getElementById('annualInput').value / 12) * 0.0145)) + (Math.round((document.getElementById('annualInput').value / 12) * 0.01)) + (Math.round((document.getElementById('annualInput').value / 12) * 0.05)) + 180;
+    // reduction values added up
+    const totalReductions = ((((document.getElementById('annualInput').value / 12) * 0.12)) + (((document.getElementById('annualInput').value / 12) * 0.07)) + (((document.getElementById('annualInput').value / 12) * 0.062)) + (((document.getElementById('annualInput').value / 12) * 0.0145)) + (((document.getElementById('annualInput').value / 12) * 0.01)) + (((document.getElementById('annualInput').value / 12) * 0.05)) + 180).toFixed(2);
     const grossResult = document.getElementById('grossResult');
-    grossResult.textContent = "$" + (document.getElementById('annualInput').value / 12).toLocaleString();
+    grossResult.textContent = "$" + ((document.getElementById('annualInput').value / 12).toFixed(2)).toLocaleString();
+    // total reductions text content
     const totalResult = document.getElementById('totalReductions');
     totalResult.textContent = "$" + totalReductions.toLocaleString();
-    const netValue = Math.round((document.getElementById('annualInput').value / 12) - totalReductions);
+    // net value and net results
+    const netValue = ((document.getElementById('annualInput').value / 12) - totalReductions).toFixed(2);
     const netResult = document.getElementById('netResult');
     netResult.textContent = "$" + netValue.toLocaleString();
 }
